addaa033b64c80df04f6cf60a6b3b8a18d9d88f1
erco <unalercann@gmail.com>
2021-01-28 14:11:10 +0300

    layout implementation

---
+++ pages/doc/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/docsLayout'
+
+export default function Docs() {
+  return (
+    <Layout>
+      <h1>Docs</h1>
+    </Layout>
+  )
+}
---
+++ pages/user/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/usersLayout'
+
+export default function Users() {
+  return (
+    <Layout>      
+      <h1>Users</h1>
+    </Layout>
+  )
+}
