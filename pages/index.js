5d07de9a1cd26106dd64762b1f7a89931d06b7be
yp <yalinpala@protonmail.com>
2021-01-29 10:51:59 +0300

    chakra ui config

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import Link from 'next/link'
-import Layout from '../components/homeLayout'
+import { Text } from "@chakra-ui/react"
+import Layout from '../components/layouts/homeLayout'
+
 
 export default function Home() {
   return (
-    <Layout>
-     
-      <h1>urcode</h1>
+    <Layout>     
+      <Text fontSize="6xl" color="pink.800">urcode</Text>
 
       <Link href="/user">
-        <a>users</a>
+        <a>
+          <Text fontSize="lg" color="pink.500">users</Text>
+        </a>
       </Link>
 
       <Link href="/doc">
-        <a>docs</a>
+        <a>
+          <Text fontSize="lg" color="pink.500">docs</Text>
+        </a>
       </Link>
 
     </Layout>
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,9 +1,10 @@
-import Layout from '../../components/usersLayout'
+import { Text } from "@chakra-ui/react"
+import Layout from '../../components/layouts/usersLayout'
 
 export default function Users() {
   return (
     <Layout>      
-      <h1>Users</h1>
+      <Text fontSize="6xl" color="pink.800">Users</Text>
     </Layout>
   )
 }
