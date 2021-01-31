2c2ef630c762415f17ddf566d0c18e41550d9502
yp <yalinpala@protonmail.com>
2021-01-31 13:15:57 +0300

    docs

---
+++ pages/docs.js
@@ -0,0 +1,36 @@
+import Layout from '../components/layouts/docsLayout'
+import NavBar from '../components/sections/navbar'
+import { getPostData } from '../lib/docs'
+
+import styles from './docs.module.css'
+
+export async function getStaticProps() {
+  const postData = await getPostData()
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Docs({ postData }) {
+  return (
+    <Layout>
+      <NavBar />
+      <section className={styles.info}>
+        <h1>
+          <span style={{color: "#1A365D"}}>* </span> 
+          urcode
+        </h1>
+        <p>
+          <span style={{fontWeight: 'bold', textDecoration: 'underline'}}>Last Update:</span>
+          <span> </span>{postData.date}
+        </p>
+      </section>
+      
+      <div className={styles.content} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+
+    </Layout>
+  )
+}
