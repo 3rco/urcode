a25292ef56cb4128b948d50881377f28f8ccb2f2
yp <yalinpala@protonmail.com>
2021-02-07 16:09:41 +0300

    darkmode button flex

--- components/sections/header.js
+++ components/sections/header.js
@@ -1,10 +1,12 @@
 import Typist from 'react-typist';
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import useDarkMode from 'use-dark-mode';
 import styles from '../sections/header.module.css'
 
 export default function Header() {
-  
   let router = useRouter()
+  const darkMode = useDarkMode(false);
 
   if (router.locale === 'en-US') {
     return (
@@ -20,6 +22,28 @@ export default function Header() {
             urcode
           </h1>
         </Typist>
+        <section>
+          {
+            darkMode.value ?
+              <a className={styles.smButton} onClick={darkMode.disable}>
+                <Image
+                    src="/img/sun.png"
+                    alt="light"
+                    width={48}
+                    height={48}                    
+                />
+              </a>
+              : 
+              <a className={styles.smButton} onClick={darkMode.enable}>
+                <Image
+                    src="/img/moon.png"
+                    alt="light"
+                    width={48}
+                    height={48}                    
+                />
+              </a>
+          }
+        </section>
       </section>
     )
   } else if (router.locale === 'tr') {
