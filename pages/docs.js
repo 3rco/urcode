5fbfa1fba9b21f7743a64cf0214351dc74775475
yp <yalinpala@protonmail.com>
2021-02-12 23:10:46 +0300

    interests

--- pages/docs.js
+++ pages/docs.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layouts/docsLayout'
 import NavBar from '../components/sections/navbar'
 import Footer from '../components/sections/footer'
+import Interest from '../components/sections/docs/interest'
 import Appearance from '../components/sections/docs/appearance'
 import FieldOfInterest from '../components/sections/docs/fieldOfInterest'
 import Lifestyle from '../components/sections/docs/lifestyle'
@@ -17,6 +18,7 @@ export default function Docs() {
     <Layout>
       <NavBar />
         <section className={styles.content}>
+          <Interest />
           <Appearance />
           <FieldOfInterest />
           <Lifestyle />
